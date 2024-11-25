6988d352833a0eea5ae83a55dd4a4465746d8fee
hendrick_BCYN <h.lincertin@bouygues-construction.com>
2024-11-25 13:11:46 +0100

    Task 3: Pushing buttons

--- unity-assets_ui/Assets/Scripts/MainMenu.cs
+++ unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -2,29 +2,36 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+// This script manages the navigation menu
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button _level01Button;
     [SerializeField] private Button _level02Button;
     [SerializeField] private Button _level03Button;
+    [SerializeField] private Button _optionsButton;
+    [SerializeField] private Button _exitButton;
 
     private void Start()
     {
         if (_level01Button != null)
-            _level01Button.onClick.AddListener(() => LevelSelect(1));
+            _level01Button.onClick.AddListener(() => LevelSelect(2));
         
         if (_level02Button != null)
-            _level02Button.onClick.AddListener(() => LevelSelect(2));
+            _level02Button.onClick.AddListener(() => LevelSelect(3));
         
         if (_level03Button != null)
-            _level03Button.onClick.AddListener(() => LevelSelect(3));
+            _level03Button.onClick.AddListener(() => LevelSelect(4));
+
+        if (_optionsButton != null)
+            _optionsButton.onClick.AddListener(() => LevelSelect(1));
+            
+        if (_exitButton != null)
+            _exitButton.onClick.AddListener(() => Debug.Log($"Exited"));
     }
 
-    public void LevelSelect(int p_level)
+    public void LevelSelect(int level)
     {
-        string sceneName = $"Level0{p_level}";
-        Debug.Log($"Loading the level: {sceneName}");
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(level);
     }
 
     private void OnDestroy()
@@ -37,5 +44,11 @@ public class MainMenu : MonoBehaviour
         
         if (_level03Button != null)
             _level03Button.onClick.RemoveAllListeners();
+
+        if (_optionsButton != null)
+            _optionsButton.onClick.RemoveAllListeners();
+
+        if (_exitButton != null)
+            _optionsButton.onClick.RemoveAllListeners();
     }
 }
