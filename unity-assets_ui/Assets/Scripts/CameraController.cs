cb2fcd3a84b7db30739c5dcddeb3d52bf8949289
hendrick_BCYN <h.lincertin@bouygues-construction.com>
2024-11-26 15:36:43 +0100

    Add camera inversion option and refactor rotation logic

--- unity-assets_ui/Assets/Scripts/CameraController.cs
+++ unity-assets_ui/Assets/Scripts/CameraController.cs
@@ -7,8 +7,10 @@ public class CameraController : MonoBehaviour
     [SerializeField] private float _height = 2f; // _height above the player
     [SerializeField] private float _rotationSpeed = 5f; 
     [SerializeField] private bool _requireRightClick = true; // If true, right-click is required to rotate
+    [SerializeField] private bool _isInverted = false; // Controls Y-axis inversion
 
-    private float _currentRotation = 0f;
+    private float _currentRotationX = 0f;
+    private float _currentRotationY = 0f;
 
     private void LateUpdate()
     {
@@ -21,21 +23,27 @@ public class CameraController : MonoBehaviour
         RotateCameraAroundPlayer();
     }
 
+    // Rotate the camera based on mouse input
     private void RotateCameraAroundPlayer()
     {
-        // Rotate the camera based on mouse input
         float l_mouseX = Input.GetAxis("Mouse X") * _rotationSpeed;
+        float l_mouseY = Input.GetAxis("Mouse Y") * _rotationSpeed;
 
         if (!_requireRightClick || Input.GetMouseButton(1)) // 1 is right mouse button
         {
-            _currentRotation += l_mouseX;
+            _currentRotationX += l_mouseX;
+            Debug.Log($"currentRotationX: {_currentRotationX}");
+
+            float l_verticalRotation = _isInverted ? -l_mouseY : l_mouseY;
+            _currentRotationY += l_verticalRotation;
+            Debug.Log($"currentRotationY: {_currentRotationY}");
         }
 
-        // Calculate the desired position
-        Vector3 l_desiredPosition = _playerPosition.position - (Quaternion.Euler(0, _currentRotation, 0) * Vector3.forward * _distance) + (Vector3.up * _height);
+        Vector3 l_direction = Quaternion.Euler(_currentRotationY, _currentRotationX, 0) * Vector3.forward;
+        Vector3 l_desiredPosition = _playerPosition.position - (l_direction * _distance) + (Vector3.up * _height);
 
         // Set the camera's position and look at the player
         transform.position = l_desiredPosition;
-        transform.LookAt(_playerPosition.position + (Vector3.up * _height / 2)); // Look at the upper body of the player
+        transform.LookAt(_playerPosition.position + (Vector3.up * _height / 2));
     }
 }
