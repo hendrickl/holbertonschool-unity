46d7a2a4667659e1b12ae916809b8d789cd9d85a
hendrick_BCYN <h.lincertin@bouygues-construction.com>
2024-11-27 13:26:02 +0100

    Add apply button functionality in OptionsMenu

--- unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -1,19 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class OptionsMenu : MonoBehaviour
 {
     [SerializeField] private Button _applyButton;
     [SerializeField] private Toggle _invertYButton;
 
+    private int _previousSceneIndex;
+
     private void Start()
     {
         bool isYaxisInverted = PlayerPrefs.GetInt("Y-axisIsInverted", 0) == 1;
         Debug.Log($"Y-axis is inverted: {isYaxisInverted}");
 
+        _previousSceneIndex = PlayerPrefs.GetInt("PreviousScene");
+        Debug.Log($"OptionsMenu - Previous scene index is {_previousSceneIndex}");
+
         if (_invertYButton != null)
             _invertYButton.isOn = isYaxisInverted;
 
         Debug.Log($"Invert button value: {isYaxisInverted}");
+
+        _applyButton.onClick.AddListener(Apply);
+    }
+
+    public void Apply()
+    {
+        PlayerPrefs.SetInt("Y-axisIsInverted", _invertYButton.isOn ? 1 : 0);
+        PlayerPrefs.Save();
+
+        Debug.Log($"OptionsMenu - Invert button value:{_invertYButton.isOn}");
+        Debug.Log($"OptionsMenu - Previous scene index is {_previousSceneIndex}");
+
+        SceneManager.LoadScene(_previousSceneIndex);
+    }
+
+    private void OnDestroy() 
+    {
+        _applyButton.onClick.RemoveListener(Apply);
     }
 }
