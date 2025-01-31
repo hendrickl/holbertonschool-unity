41723477158dd4db0a0eae9ffd73ca645f4e5c8e
hendrickl <h.lincertin@gmail.com>
2025-01-31 14:54:24 +0100

    Implement exit button for the pause menu

---
+++ 2D-clone/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private Button _restartButton;   
+    [SerializeField] private Button _resumeButton;   
+}
