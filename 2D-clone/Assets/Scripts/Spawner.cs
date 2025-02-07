4227b919223f12edfba0231f46033404e4abc42b
hendrickl <h.lincertin@gmail.com>
2025-02-07 15:43:30 +0100

    Spawner: Add floor detection

--- 2D-clone/Assets/Scripts/Spawner.cs
+++ 2D-clone/Assets/Scripts/Spawner.cs
@@ -1,12 +1,37 @@
+using System.Collections;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] _tetriminos;
     [SerializeField] private Transform _spawnPoint;
+    // [SerializeField] private GameObject _floor;
+
+    private GameObject _currentTetrimino;
 
     private void Start()
     {
+        // SpawnTetriminos();
+        StartCoroutine("SpawnTetriminoAfterDelay");
+    }
+
+    private void Update()
+    {
+        SpawnTetriminoAfterDelay();
+    }
+
+    private void OnTriggerEnter(Collider p_other)
+    {
+        if (p_other.gameObject == _currentTetrimino)
+        {
+            Debug.Log($"Spawner - OnTriggerEnter: other is {p_other}");
+            OnTetriminoLanded();
+        }
+    }
+
+    private IEnumerator SpawnTetriminoAfterDelay()
+    {
+        yield return new WaitForSeconds(2f);
         SpawnTetriminos();
     }
 
@@ -18,7 +43,20 @@ public class Spawner : MonoBehaviour
             return;
         }
 
-        int randomIndex = Random.Range(0, _tetriminos.Length);
-        Instantiate(_tetriminos[randomIndex], _spawnPoint.position, Quaternion.identity);
+        if (_currentTetrimino == null)
+        {
+            int randomIndex = Random.Range(0, _tetriminos.Length);
+            _currentTetrimino = Instantiate(_tetriminos[randomIndex], _spawnPoint.position, Quaternion.identity);
+            
+            Debug.Log($"Spawner - SpawnTetriminos: current tetrimino is {_currentTetrimino}");
+        }
+    }
+
+    private void OnTetriminoLanded()
+    {
+        _currentTetrimino = null;
+
+        Debug.Log($"Spawner - OnTetriminoLanded: current tetrimino is {_currentTetrimino}");
+        SpawnTetriminos();
     }
 }
