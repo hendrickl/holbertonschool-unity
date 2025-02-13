a1dc4f06c4d2162d291910e76368efb53ddd5c4f
hendrickl <h.lincertin@gmail.com>
2025-02-13 11:19:51 +0100

    Spawner: Add a state when the tetrimino is landed in order to spawn a new one automatically

--- 2D-clone/Assets/Scripts/Spawner.cs
+++ 2D-clone/Assets/Scripts/Spawner.cs
@@ -5,13 +5,16 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject[] _tetriminos;
     [SerializeField] private Transform _spawnPoint;
-    // [SerializeField] private GameObject _floor;
 
     private GameObject _currentTetrimino;
+    private bool _tetriminoIsLanded;
+
 
     private void Start()
     {
-        // SpawnTetriminos();
+        Debug.Log($"The tetrimino is landed : {_tetriminoIsLanded}");
+
+        SpawnTetrimino();
         StartCoroutine("SpawnTetriminoAfterDelayCoroutine");
     }
 
@@ -19,7 +22,8 @@ public class Spawner : MonoBehaviour
     {
         if (p_other.gameObject == _currentTetrimino)
         {
-            Debug.Log($"Spawner - OnTriggerEnter: other is {p_other}");
+            Debug.Log($"Spawner - OnTriggerEnter: other is {p_other.name}");
+            _tetriminoIsLanded = true;
             SpawnTetriminoWhenLanded();
         }
     }
@@ -38,20 +42,23 @@ public class Spawner : MonoBehaviour
             return;
         }
 
-        if (_currentTetrimino == null)
+        // if (_currentTetrimino.isLanded || _currentTetrimino.hasCollised)
+
+        if (_tetriminoIsLanded || _currentTetrimino == null)
         {
             int randomIndex = Random.Range(0, _tetriminos.Length);
             _currentTetrimino = Instantiate(_tetriminos[randomIndex], _spawnPoint.position, Quaternion.identity);
             
-            Debug.Log($"Spawner - SpawnTetrimino: current tetrimino is {_currentTetrimino}");
+            Debug.Log($"Spawner - SpawnTetrimino : The current tetrimino is {_currentTetrimino}");
         }
     }
 
     private void SpawnTetriminoWhenLanded()
     {
         _currentTetrimino = null;
-        Debug.Log($"Spawner - SpawnTetriminoWhenLanded: current tetrimino is {_currentTetrimino}");
+        Debug.Log($"Spawner - SpawnTetriminoWhenLanded : The current tetrimino is {_currentTetrimino}");
 
         SpawnTetrimino();
+        _tetriminoIsLanded = false;
     }
 }
