fa9f9d6ee025357b192cd22014654802608fc31a
hendrickl <h.lincertin@gmail.com>
2025-03-25 12:12:00 +0100

    Duplicate unity-assets_ui project and name it unity-animation with correct version

---
+++ unity-animation/Assets/Scripts/Timer.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _timerText;
+    [SerializeField] private TMP_Text _finalTimeText;
+    private float _elapsedTime = 0f;
+    private bool _isRunning = false;
+
+    private void Update()
+    {
+        if (_isRunning)
+        {
+            _elapsedTime += Time.deltaTime; // in seconde
+            UpdateTimer();
+        }
+    }
+
+    public void StartTimer()
+    {
+        if (!_isRunning)
+            _isRunning = true;
+    }
+
+    private void UpdateTimer()
+    {
+        float l_minutes = Mathf.FloorToInt(_elapsedTime / 60); // Mathf.FloorToInt() is a Unity function that rounds a floating point number down to the nearest integer.
+        float l_seconds = Mathf.FloorToInt(_elapsedTime % 60);
+        float l_milliseconds = (_elapsedTime % 1) * 100;
+
+        string l_timeFormat = string.Format("{0:00}:{1:00}.{2:00}", l_minutes, l_seconds, l_milliseconds);
+        _timerText.text = l_timeFormat;
+    }
+
+    public void Win()
+    {
+        Debug.Log($"Timer - called");
+        _isRunning = false;
+        _finalTimeText.text = _timerText.text;
+        Debug.Log($"Timer - Final time: {_finalTimeText.text}");
+    }
+}
---
+++ unity-animation/Assets/Scripts/WinTrigger.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using TMPro;
+
+// This script manages win condition in the game
+public class WinTrigger : MonoBehaviour
+{
+    [SerializeField] private Timer _timer;
+    [SerializeField] private Canvas _winCanvas;
+    [SerializeField] private TMP_Text _timerText;
+    [SerializeField] private float _increasedFontSize = 60f;
+    [SerializeField] private Color _winColor = Color.green;
+
+    private void Start()
+    {
+        _winCanvas.gameObject.SetActive(false)
+;    }
+
+    private void OnTriggerEnter(Collider p_other)
+    {
+        if (p_other.CompareTag("Player"))
+        {
+            DisplayWinCanvas();
+            Stop_timer();
+            IncreaseTextSize();
+            ChangeTextColor();
+            _timerText.enabled = false;
+        }
+    }
+
+    private void Stop_timer()
+    {
+        if (_timer != null)
+            _timer.enabled = false;
+    }
+
+    private void IncreaseTextSize()
+    {
+        if (_timerText != null)
+            _timerText.fontSize = _increasedFontSize;
+    }
+
+    private void ChangeTextColor()
+    {
+        if (_timerText != null)
+            _timerText.color = _winColor;
+    }
+
+    private void DisplayWinCanvas()
+    {
+        if (_winCanvas != null)
+        {
+            _winCanvas.gameObject.SetActive(true);
+            DisplayFinalTime();
+        }
+    }
+
+    private void DisplayFinalTime()
+    {
+        if (_timer != null)
+            _timer.Win();
+    }
+}
