195b9fb2ba7bd4d9be3b016867121c979b310630
hendrickl <h.lincertin@gmail.com>
2025-05-23 14:30:24 +0200

    Reset Animation development

--- unity-audio/Assets/Scripts/PlayerController.cs
+++ unity-audio/Assets/Scripts/PlayerController.cs
@@ -12,7 +12,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Vector3 _resetPosition;
     [SerializeField] private Vector3 _resetRotation;
     [SerializeField] private LayerMask _ground;
-    [SerializeField] private Animator _animator;
+    // [SerializeField] private Animator _animator;
 
     [SerializeField] private AudioSource _footstepsAudioSource;
     [SerializeField] private AudioSource _landingAudioSource;
@@ -33,10 +33,7 @@ public class PlayerController : MonoBehaviour
     private bool _playerIsRunning;
     private bool _playerIsJumping;
     private bool _playerIsFalling;
-
-    private bool _shouldTriggerFallingFlatAnimation; // Flag to track if we should trigger the animation
-    private bool _playerIsInGettingUpSequence; // Flag to track when we're in the getting up sequence
-    private bool _playerIsInFlattingFlatSequence; // Flag to track when we're in the flatting flat sequence
+    private bool _playerIsFallingFlat;
 
     private void Start()
     {
@@ -44,22 +41,14 @@ public class PlayerController : MonoBehaviour
 
         transform.position = _startPosition;
 
-        _playerHasPermissionToMove = false;
+        _playerHasPermissionToMove = true;
         _playerHasLandedOnGround = false;
         _playerWasGroundedLastFrame = false;
 
         _playerIsRunning = false;
         _playerIsJumping = false;
         _playerIsFalling = false;
-
-        _shouldTriggerFallingFlatAnimation = false;
-        _playerIsInGettingUpSequence = false;
-        _playerIsInFlattingFlatSequence = false;
-
-        Debug.Log($"PlayerController/START - Player is falling = {PlayerIsFalling()}");
-        Debug.Log($"PlayerController/START - Player is falling flat = {PlayerIsFallingFlat()}");
-
-        _animator.SetBool("isGettingUpComplete", true);
+        _playerIsFallingFlat = false;
     }
 
     private void Update()
@@ -68,20 +57,6 @@ public class PlayerController : MonoBehaviour
         if (PlayerIsGrounded() && !_playerWasGroundedLastFrame)
         {
             _playerHasLandedOnGround = true;
-            PlayLandingSFX();
-
-            // Only give permission to move if we're not in the getting up & flatting flat sequences
-            if (!_playerIsInGettingUpSequence && !_playerIsInFlattingFlatSequence)
-            {
-                _playerHasPermissionToMove = true;
-            }
-
-            // Trigger falling flat
-            if (PlayerIsFalling() && !_playerIsInGettingUpSequence)
-            {
-                _shouldTriggerFallingFlatAnimation = true;
-                StartCoroutine(TriggerFallingFlatAnimation());
-            }
         }
 
         // Detect landing from jump
@@ -92,7 +67,7 @@ public class PlayerController : MonoBehaviour
 
         _playerWasGroundedLastFrame = PlayerIsGrounded(); // Remember grounded state for the next frame
 
-        if (Input.GetKeyDown(KeyCode.Space) && PlayerIsGrounded() && _playerHasLandedOnGround && !_playerIsInGettingUpSequence)
+        if (Input.GetKeyDown(KeyCode.Space) && PlayerIsGrounded() && _playerHasLandedOnGround)
         {
             Jump();
         }
@@ -100,13 +75,12 @@ public class PlayerController : MonoBehaviour
         RotatePlayer();
         MovePlayer();
         CheckFall();
-        UpdateAnimations();
     }
 
     // Move the player based on WASD and arrows input
     private void MovePlayer()
     {
-        if (_playerHasPermissionToMove && !_playerIsInGettingUpSequence && !_playerIsInFlattingFlatSequence)
+        if (_playerHasPermissionToMove)
         {
             float verticalInput = Input.GetAxisRaw("Vertical");
 
@@ -140,7 +114,7 @@ public class PlayerController : MonoBehaviour
     // Rotate player gradually based on horizontal input 
     private void RotatePlayer()
     {
-        if (_playerHasPermissionToMove && !_playerIsInGettingUpSequence && !_playerIsInFlattingFlatSequence)
+        if (_playerHasPermissionToMove)
         {
             float horizontalInput = Input.GetAxis("Horizontal");
 
@@ -163,13 +137,7 @@ public class PlayerController : MonoBehaviour
     {
         if (transform.position.y < _fallThreshold)
         {
-            Debug.Log($"PlayerController: CHECKFALL - Player fell below threshold = {transform.position.y}");
-
-            // Set the flag to trigger falling flat animation
-            _shouldTriggerFallingFlatAnimation = true;
-
             ResetPlayerPosition();
-            StartCoroutine(TriggerFallingFlatAnimation());
         }
     }
 
@@ -177,7 +145,6 @@ public class PlayerController : MonoBehaviour
     private bool PlayerIsGrounded()
     {
         bool playerIsGrounded = Physics.CheckSphere(_groundContactPoint.position, 0.1f, _ground);
-        Debug.Log($"PlayerController: Player is grounded = {playerIsGrounded}");
         return playerIsGrounded;
     }
 
@@ -195,24 +162,7 @@ public class PlayerController : MonoBehaviour
 
     private bool PlayerIsFallingFlat()
     {
-        // Return the flag that indicates we should play the falling flat animation
-        return _shouldTriggerFallingFlatAnimation;
-    }
-
-    // Animation management
-    private void UpdateAnimations()
-    {
-        if (_animator != null)
-        {
-            _animator.SetBool("isRunning", _playerIsRunning && PlayerIsGrounded() && !PlayerIsJumping());
-            _animator.SetBool("isJumping", PlayerIsJumping());
-            _animator.SetBool("isFalling", PlayerIsFalling());
-            _animator.SetBool("isFallingFlat", PlayerIsFallingFlat());
-        }
-        else
-        {
-            Debug.Log($"PlayerController: animator not assigned");
-        }
+        return _playerIsFallingFlat;
     }
 
     // Methods to reset 
@@ -225,67 +175,7 @@ public class PlayerController : MonoBehaviour
     {
         transform.position = _resetRotation;
     }
-
-    private void ResetAnimationsState()
-    {
-        _playerIsRunning = false;
-        _playerIsJumping = false;
-        _playerIsFalling = false;
-        _shouldTriggerFallingFlatAnimation = false;
-    }
-
-    // Activate the Falling Flat animation followed by Getting Up
-    private IEnumerator TriggerFallingFlatAnimation()
-    {
-        if (_animator != null)
-        {
-            _playerIsInGettingUpSequence = false;
-            _playerIsInFlattingFlatSequence = false;
-            _playerHasPermissionToMove = false;
-
-            // Debug.Log("PlayerController/TFFA - 1: Start the Falling Flat animation");
-
-            // Step 1: Start the Falling Flat animation
-            _animator.SetBool("isFallingFlat", true);
-            _playerIsInFlattingFlatSequence = true;
-            _animator.SetBool("isGettingUpComplete", false); // Reset this to ensure proper transition
-            _animator.SetBool("isRunning", false);
-
-            // Wait for the Falling Flat Impact animation
-            yield return new WaitForSeconds(2.0f);
-
-            // Debug.Log("PlayerControlle/TFFA - 2: Falling flat animation complete -> Transitioning to Getting Up");
-
-            // Step 2: Transition to Getting Up happens automatically in the Animator, wait for its duration
-            _playerIsInFlattingFlatSequence = false;
-            _playerIsInGettingUpSequence = true;
-            yield return new WaitForSeconds(6f); // Adjust based on the Getting Up animation duration
-
-            // Debug.Log("PlayerControlle/TFFA - 3A: Getting Up animation should be complete -> Transitioning to Idle");
-
-            // Step 3: Transitioning to Idle
-            _animator.SetBool("isGettingUpComplete", true); //Set the flag to transition to Idle
-
-            // Debug.Log("PlayerControlle/TFFA - 3B: Transitioning to Idle complete");
-
-            // Reset animation flags
-            _shouldTriggerFallingFlatAnimation = false;
-            _animator.SetBool("isFallingFlat", false);
-
-            // Reset other animation states
-            ResetAnimationsState();
-
-            // End the getting up sequence and allow player to move again
-            _playerIsInGettingUpSequence = false;
-            _playerHasPermissionToMove = true;
-        }
-        else
-        {
-            Debug.Log("PlayerController: Animator not assigned");
-            yield return null;
-        }
-    }
-
+    
     // Method to detect the tag of the 2 differents ground
     private string DetectGroundTag()
     {
